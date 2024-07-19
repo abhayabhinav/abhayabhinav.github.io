f6a9c72154d6d267fc3f619268a504684d0e3e07
abhayabhinav <abhay.abhinav@gmail.com>
2024-07-19 17:33:19 -0500

    added code to get the buttons working and scene 1

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-async function init() {
-  const data = await d3.csv('https://flunky.github.io/cars2017.csv');
+const startbutton = document.querySelector('#button1');
+const scene1button = document.querySelector('#button2');
+const scene2button = document.querySelector('#button3');
+const scene3button = document.querySelector('#button4');
 
-  const svgWidth = 300;
-  const svgHeight = 300;
-  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-  const width = svgWidth - margin.left - margin.right;
-  const height = svgHeight - margin.top - margin.bottom;
+var data;
 
-  const svg = d3
-    .select('svg')
-    .attr('width', svgWidth)
-    .attr('height', svgHeight);
+const loadData = async function () {
+  data = await d3.csv('https://flunky.github.io/cars2017.csv');
+};
 
-  var x = d3.scaleLog().base(10).domain([10, 150]).range([0, width]);
-  var y = d3.scaleLog().base(10).domain([10, 150]).range([height, 0]);
+loadData();
+
+const width = 500;
+const height = 500;
+const margin = { top: 40, right: 30, bottom: 40, left: 40 };
+const svg = d3.select('svg');
+
+startbutton.addEventListener('click', function () {
+  startbutton.textContent = 'Visualization in Progress';
+  scene1button.classList.remove('hidden');
+  scene2button.classList.remove('hidden');
+  scene3button.classList.remove('hidden');
+  scene1button.classList.add('active');
+  loadScene1();
+});
+
+const loadScene1 = function () {
+  var x = d3
+    .scaleLog()
+    .base(10)
+    .domain([10, 150])
+    .range([margin.left, width - margin.right]);
+
+  var y = d3
+    .scaleLog()
+    .base(10)
+    .domain([10, 150])
+    .range([height - margin.bottom, margin.top]);
 
   svg
     .append('g')
@@ -25,20 +48,66 @@ async function init() {
     .attr('cx', function (d) {
       return x(d.AverageCityMPG);
     })
-    .attr('cy', d => y(d.AverageHighwayMPG))
-    .attr('r', d => 2 + parseInt(d.EngineCylinders));
+    .attr('cy', function (d) {
+      return y(d.AverageHighwayMPG);
+    })
+    .attr('r', function (d) {
+      return 3 + parseInt(d.EngineCylinders);
+    });
 
   svg
     .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
+    .attr('transform', `translate(0,${height - margin.bottom})`)
     .call(
-      d3.axisLeft(y).tickValues([10, 20, 50, 100]).tickFormat(d3.format('~s'))
+      d3
+        .axisBottom(x)
+        .tickValues([10, 20, 50, 100, 150])
+        .tickFormat(d3.format('~s'))
     );
 
   svg
     .append('g')
-    .attr('transform', 'translate(50,250)')
+    .attr('transform', `translate(${margin.left},0)`)
     .call(
-      d3.axisBottom(x).tickValues([10, 20, 50, 100]).tickFormat(d3.format('~s'))
+      d3
+        .axisLeft(y)
+        .tickValues([10, 20, 50, 100, 150])
+        .tickFormat(d3.format('~s'))
     );
-}
+
+  svg
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', height - margin.bottom / 4)
+    .style('text-anchor', 'middle')
+    .text('Average City MPG');
+
+  svg
+    .append('text')
+    .attr('x', -height / 2)
+    .attr('y', margin.left / 2)
+    .attr('transform', 'rotate(-90)')
+    .style('text-anchor', 'middle')
+    .text('Average Highway MPG');
+};
+
+scene1button.addEventListener('click', function () {
+  scene1button.classList.add('active');
+  scene2button.classList.remove('active');
+  scene3button.classList.remove('active');
+  loadScene1();
+});
+// Scene 2 filetr on data
+scene2button.addEventListener('click', function () {
+  scene1button.classList.remove('active');
+  scene2button.classList.add('active');
+  scene3button.classList.remove('active');
+  loadScene1();
+});
+
+scene3button.addEventListener('click', function () {
+  scene1button.classList.remove('active');
+  scene2button.classList.remove('active');
+  scene3button.classList.add('active');
+  loadScene1();
+});
